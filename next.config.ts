78e2698332d5119e4e480cb4269f32db9a93c96b
Gasto <gaston15.isma@gmail.com>
2025-09-18 10:00:46 -0300

    second

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: __dirname,
-  },
   async rewrites() {
     const backendUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api';
     
